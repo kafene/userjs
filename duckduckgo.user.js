bdbfe153cf07cb8b6d9812c678332fcdb1f057a5
kafene <kafene@users.noreply.github.com>
2014-10-28 23:47:22 -0400

    Update duckduckgo.user.js

--- duckduckgo.user.js
+++ duckduckgo.user.js
@@ -6,8 +6,8 @@
 // @match        *://*.duckduckgo.com/*
 // ==/UserScript==
 
+// Press "g" => focus search box and append "!"
 (function () {
-    // Press "g" => focus search box and append "!"
     var searchFormInput = (
         document.getElementById('search_form_input') ||
         document.getElementById('search_form_input_homepage')
@@ -43,26 +43,26 @@
             searchFormInput.focus();
         }
     });
+})();
 
-    // Add Google fallback link
-    (function () {
-        var duckbar = document.querySelector('#duckbar .zcm');
-        if (!duckbar) {
-            return;
-        }
+// Add Google fallback link
+(function () {
+    var duckbar = document.querySelector('#duckbar .zcm');
+    if (!duckbar) {
+        return;
+    }
 
-        var li = document.createElement('li');
-        var a = document.createElement('a');
-        var q = document.querySelector('#search_form_input').value;
-        var sep = document.createElement('span');
+    var li = document.createElement('li');
+    var a = document.createElement('a');
+    var q = document.querySelector('#search_form_input').value;
+    var sep = document.createElement('span');
 
-        li.classList.add('zcm__item');
-        a.classList.add('zcm__link');
-        sep.classList.add('zcm__sep--h');
-        a.textContent = 'Google';
-        a.href = 'https://encrypted.google.com/search?q=' + encodeURIComponent(q);
-        li.appendChild(a);
-        duckbar.appendChild(sep);
-        duckbar.appendChild(li);
-    })();
+    li.classList.add('zcm__item');
+    a.classList.add('zcm__link');
+    sep.classList.add('zcm__sep--h');
+    a.textContent = 'Google';
+    a.href = 'https://encrypted.google.com/search?q=' + encodeURIComponent(q);
+    li.appendChild(a);
+    duckbar.appendChild(sep);
+    duckbar.appendChild(li);
 })();

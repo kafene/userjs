1b9b7351e9f304f1ae8b70ef817b71664caa2ce0
kafene <kafene@users.noreply.github.com>
2015-01-13 15:59:33 -0500

    Update youtube-auto-pause.user.js

--- youtube-auto-pause.user.js
+++ youtube-auto-pause.user.js
@@ -1,10 +1,27 @@
+
+window.onerror = function (e) {window.console.log(e);window.alert(e);};
+if (document.domain === "kafene.org" && !window.frameElement) {
+    var xhr = new XMLHttpRequest();
+    var xhrok = function () {alert(xhr.getAllResponseHeaders());};
+    xhr.onreadystatechange = function () {this.readyState > 3 && xhrok();};
+    xhr.open("GET", "http://kafene.org");
+    xhr.send();
+}
+
 // ==UserScript==
 // @name         youtube-auto-pause
 // @version      0.1.0
 // @description  always pause youtube videos on page load
 // @namespace    http://kafene.org
 // @copyright    2014 kafene software <http://kafene.org/>
-// @match        *://*.youtube.com/*
+// @include      http://www.youtube.com/*
+// @include      https://www.youtube.com/*
+// @include      http://youtube.com/*
+// @include      https://youtube.com/*
+// @include      http://www.youtube-nocookie.com/*
+// @include      https://www.youtube-nocookie.com/*
+// @include      http://youtube-nocookie.com/*
+// @include      https://youtube-nocookie.com/*
 // @run-at       document-start
 // @grant        none
 // @license      MIT <https://raw.githubusercontent.com/kafene/userscripts/mater/LICENSE>
@@ -15,5 +32,22 @@
 
 domready(function () {
     var buttons = document.querySelectorAll(".ytp-button-pause");
-    [].forEach.call(buttons, function (button) {button.click()});
+    [].forEach.call(buttons, function (b) { b.click && b.click(); });
+});
+
+new MutationObserver(function (mutations) {
+    mutations.forEach(function (mutation) {
+        if (mutation.addedNodes) {
+            Array.from(mutation.addedNodes).forEach(function (node) {
+                if (node && node.matches && node.matches(".ytp-button-pause")) {
+                    setTimeout(function () { node.click && node.click(); });
+                }
+            });
+        }
+    });
+}).observe(document, {
+    childList: true,
+    subtree: true,
+    attributes: false,
+    characterData: false
 });

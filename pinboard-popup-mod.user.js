5cd4bd8fc996467bd3ee97618095af619d029374
kafene <kafene@users.noreply.github.com>
2015-01-13 13:27:00 -0500

    rewrite everything, should work much faster

--- pinboard-popup-mod.user.js
+++ pinboard-popup-mod.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         pinboard-popup-mod
-// @version      1.0.1
+// @version      1.1.0
 // @description  Pinboard.in bookmarklet popup page enhancements
 // @namespace    http://kafene.org
 // @match        https://*.pinboard.in/add?*
@@ -12,105 +12,111 @@
 // @updateURL    https://raw.githubusercontent.com/kafene/userscripts/master/pinboard-popup-mod.user.js
 // ==/UserScript==
 
-/*
- * On the "add bookmark" popup/page highlight assigned tags in the tag cloud,
- * and allow clicking on tags to toggle them in the tag input.
- */
+// On the "add bookmark" popup/page highlight assigned tags in the tag cloud,
+// and allow clicking on tags to toggle them in the tag input.
 domready(function () {
-    var style = document.createElement('style');
-    style.textContent = 'a.add_tag_active{background-color:rgb(221,238,255) !important;}';
-    (document.head || document.documentElement).appendChild(style);
+    // The tags input field
+    var tagsInput = document.querySelector("input[name=tags]");
 
-    var matches = function (element, selector) {
-        return (element instanceof window.Element) && (
-            window.Element.prototype.matches ||
-            window.Element.prototype.matchesSelector ||
-            window.Element.prototype.oMatchesSelector ||
-            window.Element.prototype.msMatchesSelector ||
-            window.Element.prototype.mozMatchesSelector ||
-            window.Element.prototype.webkitMatchesSelector
-        ).call(element, selector);
+    // querySelectorAll to Array helper
+    var qq = function (selector) {
+        return [].slice.call(document.querySelectorAll(selector));
     };
 
-    var input = document.querySelector('input[name=tags]');
-
-    var removeTag = function (tag) {
-        input.value = getTags().filter(function (t) {
-            return t !== tag;
-        }).join(' ');
-
-        highlight();
+    // Element matches selector
+    var matches = function (elem, selector) {
+        return elem && elem.matches && elem.matches(selector);
     };
 
-    var addTag = function (tag) {
-        input.value = getTags().concat(tag).reverse().filter(function (e, i, arr) {
-            return arr.indexOf(e, i+1) === -1;
-        }).reverse().join(' ');
-        highlight();
+    // Array unique
+    var unique = function (array) {
+        return array.reverse().filter(function (e, i, arr) {
+            return -1 === arr.indexOf(e, i+1);
+        }).reverse();
     };
 
-    var highlightNow = function () {
-        [].forEach.call(document.querySelectorAll('a[onclick*="add_tag"]'), function (a) {
-            a.classList.remove('add_tag_active');
-        });
-
-        getTags().forEach(function (tag) {
-            var taggedLinks = document.querySelectorAll('a[onclick*="add_tag(\'' + tag + '\')"]');
-            [].forEach.call(taggedLinks, function (a) {
-                a.classList.add('add_tag_active');
-                a.addEventListener('click', function (event) {
-                    var tag = event.target.textContent.trim();
-                    removeTag(tag);
-                });
-            });
-        });
-
-        var inactiveLinks = document.querySelectorAll('a[onclick*=add_tag]:not(.add_tag_active)');
-        [].forEach.call(inactiveLinks, function (a) {
-            a.addEventListener('click', function (event) {
-                var tag = event.target.textContent.trim();
-                addTag(tag);
-            });
-        });
-    };
-
+    // Get tags currently in the tags input field
     var getTags = function () {
-        return input.value.trim().split(/[,\s]+/);
+        return tagsInput.value.trim().split(/[,\s]+/).filter(function (v) { return !!v; });
     };
 
+    var ltrim = function (val) {
+        return val.replace(/^\s+/, "");
+    };
+
+    var setTags = function (newTags) {
+        tagsInput.value = ltrim(unique(newTags).sort().join(" ") + " ");
+    };
+
+    var addTagOnClick = function (event) {
+        addTag(event.target.textContent.trim());
+    };
+
+    var removeTagOnClick = function (event) {
+        removeTag(event.target.textContent.trim());
+    };
+
+    var highlightAsync = function () {
+        setTimeout(highlight);
+    };
+    
+    var removeTag = function (tag) {
+        setTags(getTags().filter(t => t !== tag));
+        highlightAsync();
+    };
+    
+    var addTag = function (tag) {
+        setTags(getTags().concat(tag));
+        highlightAsync();
+    };
+
+    var removeAddTagActiveClass = function (a) {
+        a.classList.remove("add_tag_active");
+    };
+
+    var activateTag = function (a) {
+        a.classList.add("add_tag_active");
+        a.style.backgroundColor = "rgb(221,238,255)";
+        a.removeEventListener("click", addTagOnClick, false);
+        a.addEventListener("click", removeTagOnClick, false);
+    };
+
+    var deactivateTag = function (a) {
+        a.classList.remove("add_tag_active");
+        a.style.backgroundColor = "inherit";
+        a.removeEventListener("click", removeTagOnClick, false);
+        a.addEventListener("click", addTagOnClick, false);
+    };
+
+    // Highlight a given tag (for use in higlight() loop)
+    var highlightTag = function (tag) {
+        qq('a[onclick*="add_tag(\'' + tag + '\')"]').forEach(activateTag);
+    };
+
+    // Highlight links that need to be highlighted
     var highlight = function () {
-        setTimeout(function () {
-            highlightNow();
-        });
+        qq("a[onclick*=add_tag]").forEach(removeAddTagActiveClass);
+        getTags().forEach(highlightTag);
+        qq("a[onclick*=add_tag]:not(.add_tag_active)").forEach(deactivateTag);
+        tagsInput.focus();
     };
 
-    input.addEventListener('change', highlight);
+    // Highlight when the input text changes
+    tagsInput.addEventListener("change", highlightAsync, false);
+    
+    // Highlight when a key is pressed in the input (to catch tab key autocomplete)
+    tagsInput.addEventListener("keydown", function (event) {
+        event.stopPropagation();
+        highlightAsync();
+    }, false);
 
-    input.addEventListener('keydown', function (e) {
-        e.stopPropagation();
-        highlight();
-    });
+    // Highlight when an autocomplete tag or suggested tag is clicked
+    document.addEventListener("mousedown", function (event) {
+        if (matches(event.target, "div.pin-ac li,a.suggested_tag")) {
+            highlightAsync();
+        }
+    }, false);
 
-    var observer = new MutationObserver(function (mutations, observer) {
-        mutations.forEach(function (mutation) {
-            if (mutation.addedNodes) {
-                [].forEach.call(mutation.addedNodes, function (node) {
-                    if (matches(node, 'div.pin-ac')) {
-                        node.addEventListener('mousedown', highlight);
-                    } else if (matches(node, 'a.suggested_tag')) {
-                        highlight();
-                    }
-                });
-            }
-        });
-    });
-
-    observer.observe(document.body, {
-        childList: true,
-        subtree: true,
-        attributes: false,
-        characterData: false
-    });
-
-    highlightNow();
+    // and... hightlight now (on load).
+    highlightAsync();
 });

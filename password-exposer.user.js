b7163ad295b2b3798c5973706823497b135e2fab
kafene <kafene@users.noreply.github.com>
2015-01-13 14:00:53 -0500

    Update password-exposer.user.js

--- password-exposer.user.js
+++ password-exposer.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         password-exposer
-// @version      0.0.1
+// @version      0.1.0
 // @description  Toggle password fields between text <=> password on double-click
 // @namespace    http://kafene.org
 // @copyright    2014 kafene software <http://kafene.org/>
@@ -12,12 +12,11 @@
 // @updateURL    https://raw.githubusercontent.com/kafene/userscripts/master/password-exposer.user.js
 // ==/UserScript==
 
-window.addEventListener('load', function () {
-    var fields = document.querySelectorAll('input[type="password"]');
-    [].forEach.call(fields, function (input) {
-        input.addEventListener('dblclick', function (event) {
-            var type = (this.type === 'text' ? 'password' : 'text');
-            this.setAttribute('type', this.type = type);
-        });
-    });
+document.addEventListener("dblclick", function (event) {
+    if (event.target.matches("input[type=password]")) {
+        var type = event.target.type === "text" ? "password" : "text";
+        event.target.type = type;
+        event.target.setAttribute("type", type);
+    }
 });
+

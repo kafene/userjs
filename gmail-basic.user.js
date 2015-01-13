fd563eb79fa62c322b7970affc78a9bd12912531
kafene <kafene@users.noreply.github.com>
2015-01-13 13:50:32 -0500

    Update gmail-basic.user.js

--- gmail-basic.user.js
+++ gmail-basic.user.js
@@ -13,47 +13,34 @@
 // @updateURL    https://raw.githubusercontent.com/kafene/userscripts/master/gmail-basic.user.js
 // ==/UserScript==
 
-// Add a "check all" button in HTML-Gmail's spam and trash folders.
 domready(function () {
-    var selects = document.querySelectorAll('form select[name=tact]');
-    var checkboxes = document.querySelectorAll('form td input[type=checkbox][name=t]');
-
+    // Add a "check all" button in HTML-Gmail's spam and trash folders.
+    var selects = document.querySelectorAll("form select[name=tact]");
+    var checkboxes = document.querySelectorAll("form td input[type=checkbox][name=t]");
     [].forEach.call(selects, function (select) {
-        var input = document.createElement('INPUT');
-        input.type = 'checkbox';
-
-        input.addEventListener('click', function () {
+        var input = document.createElement("input");
+        input.type = "checkbox";
+        input.addEventListener("click", function () {
             [].forEach.call(checkboxes, function (checkbox) {
-                var evt = document.createEvent('MouseEvent');
-                evt.initEvent('click', true, false);
+                var evt = document.createEvent("MouseEvent");
+                evt.initEvent("click", true, false);
                 checkbox.dispatchEvent(evt);
             });
         });
-
         select.parentNode.insertBefore(input, select);
     });
-});
 
-// Add unread count to title in Gmail Basic HTML mode
-domready(function () {
+    // Add unread count to title in Gmail Basic HTML mode
     if (!window.frameElement) {
         var originalTitle = document.title;
         var countRe = /Inbox\s+\((\d+)\)/;
         var titleRe = /\s*\((\d+)\)\s*/;
-
-        var setUnreadTitle = function () {
-            var m = countRe.exec(document.body.textContent);
-
-            if (m && m[1]) {
-                var count = '(' + parseInt(m[1], 10) + ') ';
-                document.title = count + document.title.replace(titleRe, ' ');
-            } else {
-                document.title = originalTitle;
-            }
-        };
-
-        // Update every 5 seconds
-        /*window.setInterval(setUnreadTitle, 5000);*/
-        setUnreadTitle();
+        var m = countRe.exec(document.body.textContent);
+        if (m && m[1]) {
+            var count = "(" + parseInt(m[1], 10) + ") ";
+            document.title = count + document.title.replace(titleRe, " ");
+        } else {
+            document.title = originalTitle;
+        }
     }
 });

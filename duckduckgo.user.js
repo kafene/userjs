79faefbe7621ffa2f9fe490dadd1207438752482
kafene <kafene@users.noreply.github.com>
2015-01-13 13:45:14 -0500

    Update duckduckgo.user.js

--- duckduckgo.user.js
+++ duckduckgo.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         duckduckgo
-// @version      1.0.1
+// @version      1.0.3
 // @description  Stuff for DuckDuckGo.com
 // @namespace    http://kafene.org
 // @match        *://*.duckduckgo.com/*
@@ -12,67 +12,45 @@
 // @updateURL    https://raw.githubusercontent.com/kafene/userscripts/master/duckduckgo.user.js
 // ==/UserScript==
 
-// Press "g" => focus search box and append "!"
 domready(function () {
-    var searchFormInput = (
-        document.getElementById('search_form_input') ||
-        document.getElementById('search_form_input_homepage')
-    );
-
-    if (!searchFormInput) {
-        return;
-    }
-
-    document.addEventListener('keydown', function (event) {
+    // Press "g" => focus search box and append "!"
+    var skipRegex = /^(textarea|input|select|option)$/i;
+    var searchSelector = "#search_form_input,#search_form_input_homepage";
+    document.addEventListener("keydown", function (event) {
+        var search = document.querySelector(searchSelector);
+        if (!search) { return; }
         if (document.activeElement) {
-            var activeElement = document.activeElement;
-            var activeNodeName = activeElement.nodeName ? activeElement.nodeName : '';
-
-            if (activeElement.isContentEditable ||
-                activeElement === searchFormInput ||
-                /^textarea|input|select|option$/i.test(activeNodeName))
-            {
-                return;
-            }
+            var ae = document.activeElement;
+            var nn = ae.nodeName || "";
+            if (ae === search || ae.isContentEditable || skipRegex.test(nn)) { return; }
         }
-
         var key = (event.keyCode || event.which || event.key);
-        if (!key) {
-            return;
-        }
-
-        if (
-            (220 === key || '220' === key || '\\' === key) ||
-            (71 === key  || '71'  === key || 'g'  === key)
-        ) {
+        if (key && (key == 220 || key == 71 || key == "\\" || key == "g")) {
             event.preventDefault();
             event.stopPropagation();
-            searchFormInput.value += ' !';
-            var len = searchFormInput.value.length;
-            searchFormInput.setSelectionRange(len, len);
-            searchFormInput.focus();
+            search.value += " !";
+            var len = search.value.length;
+            search.setSelectionRange(len, len);
+            search.focus();
         }
     });
-});
 
-// Add Google fallback link
-domready(function () {
-    var duckbar = document.querySelector('#duckbar .zcm');
-    if (!duckbar) {
-        return;
-    }
-
-    var li = document.createElement('li');
-    var a = document.createElement('a');
-    var q = document.querySelector('#search_form_input').value;
-    var sep = document.createElement('span');
-
-    li.classList.add('zcm__item');
-    a.classList.add('zcm__link');
-    sep.classList.add('zcm__sep--h');
-    a.textContent = 'Google';
-    a.href = 'https://encrypted.google.com/search?q=' + encodeURIComponent(q);
-    li.appendChild(a);
-    duckbar.appendChild(sep);
-    duckbar.appendChild(li);
+    // Add Google fallback link
+    (function () {
+        var duckbar = document.querySelector("#duckbar .zcm");
+        if (!duckbar) { return; }
+        var qs = "&num=100&filter=0&pws=0&safe=off&newwindow=1&spell=1&gl=us&hl=en&ie=utf8&oe=utf8";
+        var li = document.createElement("li");
+        var a = document.createElement("a");
+        var q = document.querySelector("#search_form_input").value;
+        var sep = document.createElement("span");
+        li.classList.add("zcm__item");
+        a.classList.add("zcm__link");
+        sep.classList.add("zcm__sep--h");
+        a.textContent = "Google";
+        a.href = "https://encrypted.google.com/search?q="+encodeURIComponent(q)+qs;
+        li.appendChild(a);
+        duckbar.appendChild(sep);
+        duckbar.appendChild(li);
+    })();
 });

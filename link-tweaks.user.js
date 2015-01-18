a0660db9691ff50d8e4bb9f08109d68ae68af1db
kafene <kafene@users.noreply.github.com>
2015-01-18 02:01:15 -0500

    Update link-tweaks.user.js

--- link-tweaks.user.js
+++ link-tweaks.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         link-tweaks
-// @version      1.3.1
+// @version      1.3.2
 // @description  Open External Links in New Tab, add rel=noreferrer, remove a.ping
 // @namespace    http://kafene.org
 // @include      *
